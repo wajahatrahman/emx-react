aec19bc88502c58ac8027c8d5a2fe2a613c1051f
Wajahat Rahman <wajahat.rahman@techlogix.com>
2025-02-08 23:14:26 +0500

    added more colors in the calendar + added events

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   safelist: [
     // Background colors
-    { pattern: /bg-(emerald|rose|blue|violet)-[0-9]+\/[0-9]+/ },
-    { pattern: /bg-(emerald|rose|blue|violet)-[0-9]+/ },
+    {
+      pattern:
+        /bg-(emerald|rose|blue|violet|amber|indigo|gray|pink|green|purple|orange|yellow|cyan|red|slate)-[0-9]+\/[0-9]+/,
+    },
+    {
+      pattern:
+        /bg-(emerald|rose|blue|violet|amber|indigo|gray|pink|green|purple|orange|yellow|cyan|red|slate)-[0-9]+/,
+    },
     // Text colors
-    { pattern: /text-(emerald|rose|blue|violet)-[0-9]+/ },
+    {
+      pattern:
+        /text-(emerald|rose|blue|violet|amber|indigo|gray|pink|green|purple|orange|yellow|cyan|red|slate)-[0-9]+/,
+    },
     // Border colors
-    { pattern: /border-(emerald|rose|blue|violet)-[0-9]+\/[0-9]+/ },
-    { pattern: /border-(emerald|rose|blue|violet)-[0-9]+/ },
+    {
+      pattern:
+        /border-(emerald|rose|blue|violet|amber|indigo|gray|pink|green|purple|orange|yellow|cyan|red|slate)-[0-9]+\/[0-9]+/,
+    },
+    {
+      pattern:
+        /border-(emerald|rose|blue|violet|amber|indigo|gray|pink|green|purple|orange|yellow|cyan|red|slate)-[0-9]+/,
+    },
   ],
   theme: {
     extend: {},
